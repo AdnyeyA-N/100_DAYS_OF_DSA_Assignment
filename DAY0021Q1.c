94373f58dcf40287668082a4396cb36cf9ab9e0c
AdneyaA-N <adnyeya22@gmail.com>
2026-03-01 00:28:10 +0530

    DAY0021Q1.c

---
+++ DAY0021Q1.c
@@ -0,0 +1,69 @@
+/*
+
+Problem: Create and Traverse Singly Linked List
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the result
+
+Example:
+Input:
+5
+10 20 30 40 50
+
+Output:
+10 20 30 40 50
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1) return 1;
+
+    if (n == 0) {
+        printf("\n");
+        return 0;
+    }
+
+    struct Node* head = NULL;
+    struct Node* tail = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int val;
+        scanf("%d", &val);
+
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = val;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d ", current->data);
+        struct Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+    printf("\n");
+
+    return 0;
+}
