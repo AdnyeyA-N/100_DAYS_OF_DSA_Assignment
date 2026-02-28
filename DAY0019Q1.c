565298057324fc929188f94b399ed0c894a898da
AdneyaA-N <adnyeya22@gmail.com>
2026-02-28 23:37:47 +0530

    DAY0019Q1.c

---
+++ DAY0019Q1.c
@@ -0,0 +1,82 @@
+/*
+
+Problem: Given an array of integers, find two elements whose sum is closest to zero.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the pair of elements whose sum is closest to zero
+
+Example:
+Input:
+5
+1 60 -10 70 -80
+
+Output:
+-10 1
+
+Explanation: Among all possible pairs, the sum of -10 and 1 is -9,
+             which is the closest to zero compared to other pairs.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+void findClosestToZero(int arr[], int n) {
+    if (n < 2) {
+        printf("Invalid input\n");
+        return;
+    }
+
+    qsort(arr, n, sizeof(int), compare);
+
+    int left = 0;
+    int right = n - 1;
+    int min_sum = arr[left] + arr[right];
+    int min_left = left;
+    int min_right = right;
+
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+
+        if (abs(sum) < abs(min_sum)) {
+            min_sum = sum;
+            min_left = left;
+            min_right = right;
+        }
+
+        if (sum < 0) {
+            left++;
+        } else if (sum > 0) {
+            right--;
+        } else {
+            break;
+        }
+    }
+
+    printf("%d %d\n", arr[min_left], arr[min_right]);
+}
+
+int main() {
+    int n;
+
+    if (scanf("%d", &n) != 1) return 1;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    findClosestToZero(arr, n);
+
+    return 0;
+}
